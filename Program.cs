66a8303215caa6a0565317da019c4a0999113ca6
Takayuki Hirasawa <istb16@gmail.com>
2021-08-10 11:13:04 +0900

    SvgDocumentによるsvg対応

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ using System.IO;
 using Spire.Presentation;
 using Spire.Presentation.Drawing;
 
+using Svg;
+
 namespace ImageToPptx
 {
     class Program
@@ -39,22 +41,61 @@ namespace ImageToPptx
             }
 
             // 画像を取得
-            using var img = new Bitmap(filepath);
+            Bitmap? img = null;
+            try
+            {
+                // 画像読み込み
+                img = ReadImageFile(filepath);
+                if (img == null)
+                {
+                    Console.WriteLine("Can't convert.");
+                    return;
+                }
 
-            // PowerPointを作成
-            var pptx = new Presentation();
-            // スライドのサイズを設定
-            pptx.SlideSize.Size = img.Size;
-            // スライドの背景画像を挿入
-            pptx.Slides[0].SlideBackground.Fill.FillType = FillFormatType.Picture;
-            pptx.Slides[0].SlideBackground.Fill.PictureFill.Picture.EmbedImage =
+                // PowerPointを作成
+                var pptx = new Presentation();
+                // スライドのサイズを設定
+                pptx.SlideSize.Size = img.Size;
+                // スライドの背景画像を挿入
+                var imageData = pptx.Images.Append(img);
                 pptx.Slides[0].Shapes.AppendEmbedImage(
                     ShapeType.Rectangle,
-                    filepath,
-                    new RectangleF(0, 0, img.Width, img.Height)) as IImageData;
+                    imageData,
+                    new RectangleF(0, 0, img.Width, img.Height));
+                pptx.Slides[0].SlideBackground.Fill.FillType = FillFormatType.Picture;
+                pptx.Slides[0].SlideBackground.Fill.PictureFill.Picture.EmbedImage = imageData;
 
-            // ファイルを保存
-            pptx.SaveToFile(Path.ChangeExtension(filepath, "pptx"), FileFormat.Pptx2013);
+                // ファイルを保存
+                pptx.SaveToFile(Path.ChangeExtension(filepath, "pptx"), FileFormat.Pptx2013);
+            }
+            finally
+            {
+                img?.Dispose();
+            }
+        }
+
+        static Bitmap? ReadImageFile(string filepath)
+        {
+            var ext = Path.GetExtension(filepath)?.ToLower();
+            if (ext == ".svg") return ReadSvgFile(filepath);
+            else return new Bitmap(filepath);
+        }
+
+        static Bitmap? ReadSvgFile(string filepath)
+        {
+            var doc = SvgDocument.Open(filepath);
+            if (doc == null) return null;
+            doc.Width = doc.Bounds.Width - doc.Bounds.X;
+            doc.Height = doc.Bounds.Height - doc.Bounds.Y;
+
+            // サイズ調整
+            var ratio = doc.Height / doc.Width;
+            if (doc.Width < 1280)
+            {
+                doc.Width = 1280;
+                doc.Height = doc.Width * ratio;
+            }
+            return doc.Draw();
         }
     }
 }
